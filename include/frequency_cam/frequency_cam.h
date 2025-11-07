13ecfb5d5eacfafddaacf8b285352cca415dd246
Bernd Pfrommer <bernd.pfrommer@gmail.com>
2025-11-07 17:35:37 -0500

    eventExtTrigger() returns true now

--- include/frequency_cam/frequency_cam.h
+++ include/frequency_cam/frequency_cam.h
@@ -44,7 +44,7 @@ public:
     lastEventTime_ = e.t;
     eventCount_++;
   }
-  void eventExtTrigger(uint64_t, uint8_t, uint8_t) override {}
+  bool eventExtTrigger(uint64_t, uint8_t, uint8_t) override { return (true); }
   void finished() override {}
   void rawData(const char *, size_t) override {}
   // ------------- end of inherited from EventProcessor
